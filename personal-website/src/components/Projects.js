356cb356ddb1fe8ab4146ea04964109db936ab08
Jeffrey Chang <jeffreyc@kth.se>
2025-07-03 12:03:11 +0200

    final touches

--- personal-website/src/components/Projects.js
+++ personal-website/src/components/Projects.js
@@ -7,32 +7,38 @@ function Projects() {
     {
       title: 'Unity3D Game Development',
       description: 'A project involving the development of a football game with powerups, AI opponents, and multiplayer functionality using Unity3D and C#.',
-      image: 'https://via.placeholder.com/400x250/FF5733/FFFFFF?text=Project+1'
+      image: '/pictures/project1.png',
+      link: 'https://noahpe0.wixsite.com/klantboll/game'
     },
     {
       title: 'Dynamic Web Application',
       description: 'A project in frontend development and interaction programming using React and JavaScript and a ChatGPT and Pokemon API integration.',
-      image: 'https://via.placeholder.com/400x250/33FF57/FFFFFF?text=Project+2'
+      image: '/pictures/project2.png',
+      link: 'https://pokeme-dh2642.web.app/'
     },
     {
       title: 'Old and Current Website',
       description: 'This and the previous website made for fun and personal use.',
-      image: 'https://via.placeholder.com/400x250/3357FF/FFFFFF?text=Project+3'
+      image: '/pictures/project3old.png',
+      link: '#'
     },
     {
       title: 'Ecommerce Website',
       description: 'A project where I developed a full-stack ecommerce website using React, Node.js, and MongoDB, implementing features like user authentication, product management, and payment processing.',
-      image: 'https://via.placeholder.com/400x250/F333FF/FFFFFF?text=Project+4'
+      image: '/pictures/project4.png',
+      link: 'https://homeearths.com/'
     },
     {
       title: 'Bachelor Thesis Project',
       description: 'My bachelor thesis project focused on the comparative analysis of different quantization algorithms for image classification, where we implemented and tested various models to determine their effectiveness.',
-      image: 'https://via.placeholder.com/400x250/FFC300/FFFFFF?text=Project+5'
+      image: '/pictures/project5copy.jpeg',
+      link: 'https://kth.diva-portal.org/smash/record.jsf?pid=diva2%3A1887665&dswid=9194'
     },
     {
       title: 'AI Model Creation',
       description: 'A hobby project where I created an AI model to generate images based on text prompts.',
-      image: 'https://via.placeholder.com/400x250/C70039/FFFFFF?text=Project+6'
+      image: '/pictures/project6.jpg',
+      link: 'https://github.com/zwjc'
     },
   ];
 
@@ -42,15 +48,19 @@ function Projects() {
       <div className="project-cards-grid">
         {projects.map((project, index) => (
           <ScrollAnimationWrapper key={index} animationClass="fade-in-up" delay={index * 0.1}>
-            <div
-              className="project-card"
-              style={{ backgroundImage: `url(${project.image})` }}
-            >
-              <div className="project-card-overlay">
-                <h3>{project.title}</h3>
-                <p>{project.description}</p>
+            <a href={project.link} target="_blank" rel="noopener noreferrer" className="project-card-link">
+              <div
+                className="project-card"
+                style={{ backgroundImage: `url(${project.image})` }}
+              >
+                <div className="project-card-title">
+                  <h3>{project.title}</h3>
+                </div>
+                <div className="project-card-overlay">
+                  <p>{project.description}</p>
+                </div>
               </div>
-            </div>
+            </a>
           </ScrollAnimationWrapper>
         ))}
       </div>
