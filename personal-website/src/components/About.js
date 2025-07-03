1638d1acf79e80dd0e124d53042b49f7abac31c6
Jeffrey Chang <jeffreyc@kth.se>
2025-07-03 11:46:21 +0200

    added favicon

--- personal-website/src/components/About.js
+++ personal-website/src/components/About.js
@@ -12,23 +12,32 @@ function About() {
       </ScrollAnimationWrapper>
 
       <ScrollAnimationWrapper animationClass="fade-in-up delay-1">
-        <section className="about-section">
-          <h2>Contact Information</h2>
-          <p>
-            <div style={{ display: 'flex', gap: '1.5rem'}}>
+        <div className="contact-info">
+          <div style={{ display: 'flex', gap: '1.5rem'}}>
             <a href="https://www.linkedin.com/in/zwjc" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn Profile">
-              <i className="fab fa-linkedin" style={{ fontSize: '2rem', color: '#0077B5' }}></i>
+              <i className="fab fa-linkedin" style={{ fontSize: '2.5rem', color: '#0077B5' }}></i>
             </a>
             <a href="https://www.github.com/zwjc" target="_blank" rel="noopener noreferrer" aria-label="GitHub Profile">
-              <i className="fab fa-github" style={{ fontSize: '2rem', color: '#333' }}></i>
+              <i className="fab fa-github" style={{ fontSize: '2.5rem', color: '#333' }}></i>
             </a>
-            </div>
-          </p>
+          </div>
+        </div>
+      </ScrollAnimationWrapper>
+
+      <ScrollAnimationWrapper animationClass="fade-in-up delay-2">
+        <section className="about-section">
+          <h2>Experience</h2>
+          <h3>Project Manager @ THS Armada</h3>
+          <p><em>January 2024 - January 2025</em></p>
+          <ul>
+            <li>Responsible for planning and coordinating the work of over 200 volunteers and 140 companies with a budget of 7MSEK.</li>
+            <li>Main contact person for external representatives including businesses, state agencies and universities.</li>
+          </ul>
         </section>
       </ScrollAnimationWrapper>
 
 
-      <ScrollAnimationWrapper animationClass="fade-in-up delay-2">
+      <ScrollAnimationWrapper animationClass="fade-in-up delay-4">
         <section className="about-section">
           <h2>Education</h2>
           <h3>KTH - MSc Industrial Engineering and Management</h3>
